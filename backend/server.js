30b795667f60216f93c0594a533ebb2294dcea7f
Kunal <kunalpa239@gmail.com>
2022-04-19 23:56:07 +0530

    proc file added

--- backend/server.js
+++ backend/server.js
@@ -24,14 +24,18 @@ app.use("/api/notes", noteRoutes);
 
 //--------------deployment---------
 
-// __dirname = path.resolve()
-// if(process.env.NODE_ENV==='production'){
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-// }else{
-//   app.get("/", (req, res) => {
-//     res.send("api is running");
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("api is running");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
